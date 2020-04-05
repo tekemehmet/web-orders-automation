a7277407c0c886e5ed8b401c3e7b5e36b2893817
tekemehmet <tekemehmet@yahoo.com>
2020-04-05 16:32:17 -0400

    Added AbstractBaseTest class

--- src/test/java/com/weborders/pages/AbstractBasePage.java
+++ src/test/java/com/weborders/pages/AbstractBasePage.java
@@ -25,6 +25,10 @@ public abstract class AbstractBasePage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
+    /**
+     * Specify component name as a parameter, like: View all products or Orders
+     * @param component
+     */
     public void navigateTo(String component){
         String locator = "//a[text()='"+component+"']";
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator))).click();
